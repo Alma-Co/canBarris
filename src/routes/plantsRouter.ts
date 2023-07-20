36ca8ff9f157373334a8c41be04d31e9bf9f556b
Alma-Co <edith.cantegrit@gmail.com>
2023-07-20 17:15:45 +0200

    update plantsRouter and tsc watch

--- src/routes/plantsRouter.ts
+++ src/routes/plantsRouter.ts
@@ -5,27 +5,32 @@ const router = Router();
 
 router.get('/plants', async(req: express.Request, res: express.Response) => {
     let plants = await new Plants().getPlants();
+    return res.json(plants);
 });
 
 router.get('/plants/:id', async(req: express.Request, res: express.Response) => {
     let { id } = req.params;
     let plant = await new Plants().getPlantById(id);
+    return res.json(plant);
 });
 
 router.post('/plants', async(req: express.Request, res: express.Response) => {
     let { name, type, light, water, comment } = req.body;
     let plant = await new Plants().createPlant({ name, type, light, water, comment });
+    return res.json(plant);
 });
 
 router.put('/plants/:id', async(req: express.Request, res: express.Response) => {
     let { id } = req.params;
     let { name, type, light, water, comment } = req.body;
     let plant = await new Plants().updatePlant({ id, name, type, light, water, comment });
+    return res.json(plant);
 });
 
 router.delete('/plants/:id', async(req: express.Request, res: express.Response) => {
     let { id } = req.params;
     let plant = await new Plants().deletePlant(id);
+    return res.json(plant);
 });
 
 export default router;
